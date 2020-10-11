8061cbedcc313b4729e1e6640cecad01b79010fa
Aurélie Mlynarz <aureliemlynarz@macbook-pro-de-aurelie.home>
2020-10-11 12:12:52 +0200

    ajout routes auth

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const path = require('path');
 
+// import des routes
+const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 // lancement de l'application express
 const app = express();
 
+// gestion CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -26,12 +29,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./models");
 db.sequelize.sync();
 
-// simple route
+// TEST route a supprimer
 app.get("/", (req, res) => {
   res.json({ message: "Welcome lilimly's application." });
 });
 
-// route get all users
+// enregistrement des routeurs
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 // export de notre app
