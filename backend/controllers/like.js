3c6973906dd25ef979308aa91f33934575d76b8c
Aurélie Mlynarz <aureliemlynarz@macbook-pro-de-aurelie.home>
2020-10-27 19:37:59 +0100

    ajout /suppr like

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -15,16 +15,13 @@ exports.findAllLikes = (req, res, next) => {
 // logique métier : créer un like
 exports.createLike = (req, res, next) => {
   const likeObject = req.body;
-  Like.findAll({where: {
-    articleId: req.params.id}})
-    .then(likes => {
-        if (likes.userId.includes(req.body.userId)){
-          Like.destroy({ where: {
-            articleId: req.params.id,
-            userId: req.body.userId }})
-              .then(() => res.status(200).json({ message: 'Like supprimé !'}))
-              .catch(error => res.status(400).json({ error }));
-        } else {
+    Like.findAll({where: {
+      articleId: req.body.articleId,
+      userId: req.body.userId
+      }})
+      .then(likes => {
+        console.log(likes.length)
+        if(likes.length === 0) {
           const like = new Like({
             ...likeObject
           });
@@ -32,46 +29,13 @@ exports.createLike = (req, res, next) => {
           like.save()
             .then(() => res.status(201).json({ message: 'Like ajouté !'}))
             .catch(error => res.status(400).json({ error }));
+        } else {
+          Like.destroy({ where: {
+            articleId: req.body.articleId,
+            userId: req.body.userId }})
+              .then(() => res.status(200).json({ message: 'Like supprimé !'}))
+              .catch(error => res.status(400).json({ error }));
         }
-   }
-  )
-  .catch(error => res.status(400).json({ error }));
+      }
+    )
 }
-
-
-
-/*if (req.body.like === 1) {
-    const like = new Like({
-      ...likeObject
-    });
-    // Enregistrement de l'objet like dans la base de données
-    like.save()
-      .then(() => res.status(201).json({ message: 'Like ajouté !'}))
-      .catch(error => res.status(400).json({ error }));
-  } else if (req.body.like === 0) {
-      Like.destroy({ where: {
-        articleId: req.body.articleId,
-        userId: req.body.userId }})
-            .then(() => res.status(200).json({ message: 'Like supprimé !'}))
-            .catch(error => res.status(400).json({ error }));
-  }; */
-
-
-// logique métier : lire un like par son id
-exports.findOneLike = (req, res, next) => {
-  Like.findOne({ 
-    where: {id: req.params.id } })
-  .then(like => {
-    console.log(like);
-    res.status(200).json(like)
-  })
-  .catch(error => res.status(404).json({ error }));
-};
-
-
-// Logique métier : supprimer un like
-exports.deleteLike = (req, res, next) => {
-  Like.destroy({ where: {id: req.params.id} })
-        .then(() => res.status(200).json({ message: 'Like supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
-};
--- backend/routes/like.js
+++ backend/routes/like.js
@@ -5,8 +5,6 @@ const likeCtrl = require('../controllers/like');
 
 // Routes
 router.get('/', likeCtrl.findAllLikes);
-router.get('/:id', likeCtrl.findOneLike);
 router.post('/', likeCtrl.createLike);
-router.delete('/:id', likeCtrl.deleteLike);
 
 module.exports = router;
